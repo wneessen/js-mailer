4944ec2830769053ac22a12af84e80051805a9f5
Winni Neessen <wn@neessen.dev>
2025-12-14 19:15:23 +0000

    Added HTTP middleware and routing setup
    - Implemented `routes` method to configure HTTP middleware and register API routes
    - Added middleware for RealIP, StripSlashes, compression, and structured request logging
    - Registered `/ping` and `/token` endpoints with corresponding handlers
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

---
+++ internal/server/routes.go
@@ -0,0 +1,49 @@
+// SPDX-FileCopyrightText: Winni Neessen <wn@neessen.dev>
+//
+// SPDX-License-Identifier: MIT
+
+package server
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"strings"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/httplog/v3"
+)
+
+func (s *Server) routes(_ context.Context) error {
+	logFormat := httplog.SchemaECS
+	logSkipPath := []string{"/skip"}
+	logger := s.log.With(slog.String("service", "http"))
+	logHandler := httplog.RequestLogger(
+		logger,
+		&httplog.Options{
+			Level: s.config.Log.Level,
+			Skip: func(req *http.Request, code int) bool {
+				for _, skip := range logSkipPath {
+					if strings.HasPrefix(req.URL.Path, skip) && code == 200 {
+						return true
+					}
+				}
+				return false
+			},
+			Schema:        logFormat,
+			RecoverPanics: true,
+		},
+	)
+
+	// Register middleware
+	s.mux.Use(middleware.RealIP)
+	s.mux.Use(middleware.StripSlashes)
+	s.mux.Use(middleware.Compress(5))
+	s.mux.Use(logHandler)
+
+	// Register routes
+	s.mux.Get("/ping", s.HandlerAPIPingGet)
+	s.mux.Post("/token", s.HandlerAPITokenPost)
+
+	return nil
+}
