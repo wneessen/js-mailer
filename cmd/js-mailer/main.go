0cdef54fffa527cba4146ad5f72279dc25ce7d5c
Winni Neessen <wn@neessen.dev>
2025-12-14 19:12:14 +0000

    Initial implementation of js-mailer v2 service entry point
    - Added `main.go` to define the entry point of the service
    - Implemented configuration loading with support for default and file-based configs
    - Introduced signal handling for graceful shutdown using `context`
    - Integrated logger initialization with configurable log level
    - Set up and started the js-mailer server instance with error handling
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

---
+++ cmd/js-mailer/main.go
@@ -0,0 +1,67 @@
+// SPDX-FileCopyrightText: Winni Neessen <wn@neessen.dev>
+//
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"syscall"
+
+	"github.com/wneessen/js-mailer/internal/config"
+	"github.com/wneessen/js-mailer/internal/logger"
+	"github.com/wneessen/js-mailer/internal/server"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGKILL,
+		syscall.SIGABRT, os.Interrupt)
+	defer cancel()
+
+	// We start with a very basic logger
+	log := logger.New(slog.LevelError)
+
+	// Read default config
+	conf, err := config.New()
+	if err != nil {
+		log.Error("failed to load config", logger.Err(err))
+		os.Exit(1)
+	}
+
+	// Check if config file was specified
+	confPath := flag.String("config", "", "path to the config file")
+	flag.Parse()
+	if *confPath != "" {
+		file := filepath.Base(*confPath)
+		path := filepath.Dir(*confPath)
+		conf, err = config.NewFromFile(path, file)
+		if err != nil {
+			log.Error("failed to load config from file", logger.Err(err))
+			os.Exit(1)
+		}
+	}
+
+	// Initialize a new logger with the config
+	log = logger.New(conf.Log.Level)
+
+	// Initalize server instance
+	srv := server.New(conf, log)
+
+	// Start server
+	log.Info("starting js-mailer service")
+	if err = srv.Start(ctx); err != nil {
+		log.Error("failed to start server", logger.Err(err))
+	}
+	log.Info("shutting down js-mailer service")
+}
