49e46aca19c0413cc8a722b2c1eb971189d44bb8
Winni Neessen <wn@neessen.dev>
2025-12-17 18:30:52 +0000

    Enhanced CORS preflight check and improved logging
    - Added detailed logging for missing form IDs, failed form loading, and invalid origins
    - Refactored CORS handling to enforce stricter checks and return `403 Forbidden` for disallowed origins
    - Improved CORS header management for preflight requests, ensuring correct `Access-Control-Allow` response behavior
    
    Signed-off-by: Winni Neessen <wn@neessen.dev>

--- internal/server/middleware_cors.go
+++ internal/server/middleware_cors.go
@@ -6,34 +6,40 @@ package server
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
 	"strings"
 
 	"github.com/go-chi/chi/v5"
 
 	"github.com/wneessen/js-mailer/internal/forms"
+	"github.com/wneessen/js-mailer/internal/logger"
 )
 
 const AccessControlMaxAge = "600"
 
 func (s *Server) preflightCheck(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if (r.Method != http.MethodOptions && r.Method != http.MethodPost) ||
-			r.Header.Get("Origin") == "" ||
-			r.Header.Get("Access-Control-Request-Method") == "" {
-
+		origin := r.Header.Get("Origin")
+		if origin == "" {
 			next.ServeHTTP(w, r)
 			return
 		}
 
 		formID := chi.URLParam(r, "formID")
+		if formID == "" {
+			s.log.Warn("missing form ID", slog.Any("headers", r.Header), slog.String("origin", origin),
+				slog.String("path", r.URL.Path), slog.String("method", r.Method))
+			next.ServeHTTP(w, r)
+			return
+		}
 		form, err := forms.New(s.config.Forms.Path, formID)
 		if err != nil || form == nil {
+			s.log.Error("failed to load form configuration", logger.Err(err), slog.String("formID", formID))
 			next.ServeHTTP(w, r)
 			return
 		}
 
-		origin := r.Header.Get("Origin")
 		allowedDomain := false
 		for _, domain := range form.Domains {
 			if strings.EqualFold(origin, fmt.Sprintf("https://%s", domain)) {
@@ -42,15 +48,24 @@ func (s *Server) preflightCheck(next http.Handler) http.Handler {
 			}
 		}
 		if !allowedDomain {
-			next.ServeHTTP(w, r)
+			s.log.Warn("origin not allowed", slog.String("origin", origin), slog.String("form", formID))
+			w.WriteHeader(http.StatusForbidden)
 			return
 		}
 
-		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+		// must be set for all CORS responses
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Vary", "Origin")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
-		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
-		w.Header().Set("Access-Control-Max-Age", AccessControlMaxAge)
-		w.WriteHeader(http.StatusNoContent)
+
+		// Set CORS headers for preflight requests
+		if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+			w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+			w.Header().Set("Access-Control-Max-Age", AccessControlMaxAge)
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
 	})
 }
